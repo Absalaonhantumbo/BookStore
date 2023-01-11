c6a8655349fa90bc13d913867bcb0dfe3e6f5243
Absalao Nhantumbo <absalaonhantumbo1@gmail.com>
2023-01-11 13:47:38 +0200

    feat: Add new table Custumer buys book

---
+++ Domain/CustomerBuysBook.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain;
+
+public class CustomerBuysBook : BaseEntity
+{
+    public int BookId { get; set; }
+    [ForeignKey("BookId")] public Book Book { get; set; }
+    
+    public int CostumerId { get; set; }
+    [ForeignKey("CostumerId")] public Costumer Costumer { get; set; }
+
+    public int Amount { get; set; }
+    
+    public double SaleValue { get; set; }
+    
+    public DateTime DateOfSale { get; set; } = DateTime.UtcNow;
+}
